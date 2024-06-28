cd50b163d46e5ad4fc0b736007b19c355c9f1cd5
Marie Severa <maja.severa@gmail.com>
2024-06-28 23:20:32 +0200

    final version

--- main.py
+++ main.py
@@ -2,34 +2,29 @@ import datetime
 import pygame
 from tkinter import *
 
-# colors
+# colors and font
 purple = '#80247f'
 turquoise = '#005f64'
 white = '#fff'
-
+main_font = 'Arial'
 
 pygame.init()
 sound = pygame.mixer.Sound('sound/sound.wav')
 
-main_font = 'Arial'
-
 window = Tk()
-window.geometry('300x150')
+window.geometry('280x140')
 window.config(bg=purple)
 window.iconbitmap('icon/icon2.ico')
 window.title('Alarm clock')
 
-
-
-# funkce tlacitek
+# funkcions - sound
 def sound_play():   
     sound.play()
 
 def sound_stop():
     sound.stop()
 
-
-# funkce labelu cas
+# funkcions - time
 def get_current_time():
     actuell_time = datetime.datetime.now()
     return actuell_time.strftime("%H:%M:%S")
@@ -39,13 +34,11 @@ def update_time():
     current_time.config(text=actuel_time) 
     window.after(1000, update_time)
 
-# funkce na zazvoneni
-
+# funkcion - time compare and ring
 def user_input():
     hh = hours.get()
     mm = minutes.get()
     return f'{hh}:{mm}:00'
-
 
 def time_compare():
     user = user_input()
@@ -53,13 +46,12 @@ def time_compare():
     if user == current:
         sound_play()
 
-# hlavni smycka 
-
+# main loop
 def update_time_compare():
     time_compare()
     window.after(1000, update_time_compare)
 
-# funkce pro snooze
+# snooze
 def snooze_button():
     sound_stop()
     snooze_time = (datetime.datetime.now() + datetime.timedelta(minutes=5)).strftime("%H:%M:%S")
@@ -68,8 +60,7 @@ def snooze_button():
     minutes.delete(0, END)
     minutes.insert(0, snooze_time[3:5])
 
-    
-# aktualni cas
+# Label - current time
 current_time = Label(window, bg=purple, fg=white)
 current_time.place(x= 70, y= 80)
 
@@ -85,16 +76,13 @@ colon.place(x=85, y=30)
 minutes = Entry(window, justify=CENTER, font=(main_font, 15), width=4)
 minutes.place(x=105, y=30)
 
-
-
-#!! vytvorit funkci na pripocitani 5 minut a spusteni znovu
+# Button - Snooze
 button_snooze = Button(window, text='Snooze', command=snooze_button, bg=turquoise, fg=white, width=7)
 button_snooze.place(x=190, y=30)
 
-# vypnuti budiku
+# Button - Stop
 button_stop = Button(window, text='Stop', command=sound_stop, bg=turquoise, fg=white, width=7)
 button_stop.place(x=190, y=80)
-
 
 update_time()
 update_time_compare()
